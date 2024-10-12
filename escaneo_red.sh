6c2f32d8ac2101d6db12a0bd6a39f44fee6ecefd
yeeiisi <127243820+yeeiisi@users.noreply.github.com>
2024-10-12 18:13:20 -0400

    Update escaneo_red.sh

--- escaneo_red.sh
+++ escaneo_red.sh
@@ -6,6 +6,29 @@ ROJO='\033[38;5;160m'
 MAGENTA='\033[1;33m'
 RESET='\033[0m'
 
+show_watermark() {
+    echo -e "\e[38;5;196m"
+    echo "==========================================="
+    banner_text="Y3i\$i"
+
+    banner_output=$(figlet -k "$banner_text")
+    tabulation="        "
+    echo "$banner_output" | while IFS= read -r line; do
+        echo -e "${tabulation}${line}"
+    done
+
+    echo
+    echo "Github: https://github.com/yeeiisi"
+    echo "Telegram: @yeeiisi"
+    echo
+    echo "==========================================="
+
+    echo -e "\e[0m"
+}
+
+# Mostrar marca de agua
+show_watermark
+
 # Verificar el uso del script
 if [ "$#" -ne 1 ]; then
     echo -e "${ROJO}Uso: $0 <RED_LOCAL>${RESET}"
@@ -24,7 +47,7 @@ for i in $(seq $RANGO_INICIO $RANGO_FIN); do
     # nc para comprobar si el puerto 22 está abierto
     nc -z -w 1 $IP 22 2>/dev/null
     if [ $? -eq 0 ]; then
-        echo -e "${VERDE}Puerto 22 abierto en $IP${RESET}"
+        echo -e "${GREEN}Puerto 22 abierto en $IP${RESET}"
     fi
 
     # nc para comprobar si el puerto 80 está abierto
